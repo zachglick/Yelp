2f26f6835747e9f8c99f67d4707697ac2b06b69d
zachglick <glickzachary@gmail.com>
2016-02-12 14:32:06 -0600

    Began Setup

---
+++ Yelp/BusinessCell.swift
@@ -0,0 +1,39 @@
+//
+//  BusinessCell.swift
+//  Yelp
+//
+//  Created by Zach Glick on 2/11/16.
+//  Copyright © 2016 Timothy Lee. All rights reserved.
+//
+
+import UIKit
+
+class BusinessCell: UITableViewCell {
+
+    @IBOutlet weak var thumbImageView: UIImageView!
+    @IBOutlet weak var ratingImageView: UIImageView!
+    @IBOutlet weak var reviewsCountLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var distanceLabel: UILabel!
+    @IBOutlet weak var categoriesLabel: UILabel!
+    
+    var business : Business! {
+        didSet {
+                nameLabel.text = business.name
+        }
+    }
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
